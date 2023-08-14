ced779f3fc9bd5f0d145b9da94f297c2cd54078f
Vitor Veloso <vitorvelosodev@gmail.com>
2023-08-14 14:24:13 -0300

    :sparkles: Angular todo list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from './Todo';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-list';
+  todos: Todo[] = [];
+  newTodo: string;
+
+  saveTodo() {
+    if (!this.newTodo) return;
+    const todo = new Todo();
+    todo.name = this.newTodo;
+    todo.isCompleted = false;
+    this.todos.push(todo);
+    this.newTodo = '';
+  }
+
+  done(id: number) {
+    this.todos[id].isCompleted = !this.todos[id].isCompleted;
+  }
+
+  remove(id: number) {
+    this.todos = this.todos.filter(todo => this.todos[id] !== todo);
+  }
 }
